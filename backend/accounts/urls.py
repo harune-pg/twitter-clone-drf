b3237854b625f83001ef58a99caa1ad328781706
harune-pg <h.makino.22.6.533@4nonome.com>
2024-08-28 21:49:35 +0900

    add: accounts test

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,4 +1,7 @@
-from django.urls import path, include
+from django.urls import path
+
+from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
+# from dj_rest_auth.jwt_auth import get_refresh_view
 
 from . import views
 
@@ -6,6 +9,11 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', include('dj_rest_auth.urls')),
-    path('signup/', views.SignUpView.as_view(), name='singup'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    # TODO: あとで
+    # path('token/refresh/', get_refresh_view().as_view(), name='token_refresh'),
+    # FIXME: ログイン確認用（暫定）
+    path('user/', UserDetailsView.as_view(), name='rest_user_details'),
 ]
